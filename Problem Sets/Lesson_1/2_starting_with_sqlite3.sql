95574ae304c8626a148259fd5eaab7f522d30add
jdnyzio <joe.nyzio@udacity.com>
2015-10-16 03:02:13 +0000

    Starting with SQlite

--- Problem Sets/Lesson_1/2_starting_with_sqlite3.sql
+++ Problem Sets/Lesson_1/2_starting_with_sqlite3.sql
@@ -1,37 +1,42 @@
-#After downloading SQLite3, you'll have the chance to run many of the class exercises locally.
+#After downloading everything from the last slide, you're
+now the proud owner of a local SQLite environment!
 
-#This will help prepare you for the skills you'll need to complete the project later on, 
-#and give you confidence managing your own local environment.  Here are a few simple commands 
-#to help get you started.  
+#Here are some important commands you can use to help explore your
+new toys.
 
-#Type the following commands directly into your terminal window to get started using SQLite3.
+#Type the following commands into your terminal window to get started using SQLite3.
 
-#Navigate to the class_db folder
-cd class_db
-#Look at the databases
+#Take a peek at your new class_db folder through your terminal window.
+cd your/file/path/class_db
 ls
-#Go into the animals database
-sqlite3 animals.db
-#Check out tables
-.tables
-#Look at the animals table
-SELECT * FROM animals;
-#Make it a little prettier
-.header on #show header
-.mode column #break into columns
-#Let's look again
-SELECT * FROM animals;
-#What about the other tables?
-.tables
-SELECT * FROM diet;
 
-#Getting to a New database
+#Let's log into the Chinook_Sqlite database
+sqlite3 Chinook_Sqlite.sqlite
+
+#What tables do you see in the database?
+.tables
+
+#Could you tell me what data is in the Invoice table?
+SELECT * FROM Invoice;
+#Thanks so much!
+
+#Try another table.
+SELECT * FROM instruments;
+#Whoops, what happened!?
+.tables
+
+#Looks like instruments isn't actually a table in the database.  
+#No harm done though.  Don't be afraid to try new things :)
+
+#Have a look at some of the other tables just how you did with the Invoice table.
+SELECT * FROM tableName;
+#Go crazy, explore it all!
+
+#When you're all done exploring you can leave the database.
 .exit 
-ls 
-#Let's look at mooseball
-sqlite3 mooseball.db
-.tables
-SELECT * FROM players;
+#Goodbye friend.
 
-#These commands are a simple way to get started using your new SQLite environment.
-#Try to continue experimenting locally while progressing through the course.
+#Can we query the table from outside the database?
+SELECT * FROM Album;
+#No you can't! Great question though.  Stay curious.
+
