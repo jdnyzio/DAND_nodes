cbb49c9a81a677fee1b4ddf7c56ff3730f0ce8aa
jdnyzio <joe.nyzio@udacity.com>
2015-10-20 19:44:23 +0000

    iron

--- Problem Sets/Lesson_3/1a_invoice_subquery.sql
+++ Problem Sets/Lesson_3/1a_invoice_subquery.sql
@@ -5,7 +5,10 @@
 #For this problem, create a query that shows the city, stat, country, and total of customers whose 
 #invoice totals are above the average invoice total.
 
-SELECT BillingCity, BillingState,  BillingCountry, Total 
+SELECT BillingCity, 
+       BillingState,  
+       BillingCountry, 
+       Total 
 FROM Invoice, 
     (SELECT avg(Total) as av 
      FROM Invoice) as subq 
