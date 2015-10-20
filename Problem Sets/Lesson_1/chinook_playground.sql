5a400a6cc7a630cf2f0ba745e11f748409ebfe6a
jdnyzio <joe.nyzio@udacity.com>
2015-10-20 02:12:17 +0000

    playground

--- Problem Sets/Lesson_1/chinook_playground.sql
+++ Problem Sets/Lesson_1/chinook_playground.sql
@@ -1,31 +1,37 @@
-#At the heart of SQL is the ability to translate your
-and questions into a language that a database can understand.
+#At the heart of any SQL query is the ability to translate your
+#thoughts into a language that your database can understand.
 
-#As you learn to build more complex queries, you'll find that they closely 
-#resemble how you originally thought about the question in your own language.
+#As you learn to build queries, you'll find that the language 
+#closely resembles the question as you had originally thought 
+#of it in your native languge.
 
-#Try translating the following queries into the questions they were meant to answer. 
+#See if you can translate the following queries into the questions 
+#they were meant to answer. 
 
 #You can also run these queries in the browser or in the local environment you've 
 #set up to see if they return what you expected!
 
-#Example
-#What songs in this dataset were written by Van Halen?
+
+#QUESTION: What songs in this dataset were written by Van Halen?
 SELECT Name FROM Track WHERE Composer='Van Halen';
 
-#Track
-
+#QUESTION:
 SELECT Name FROM Track WHERE Composer="AC/DC"; 
 
+#QUESTION:
 SELECT Composer, COUNT(*) FROM Track WHERE Composer='Queen';
 
+#QUESTION:
 SELECT Composer, SUM(Milliseconds), SUM(Bytes) FROM Track WHERE Composer='Miles Davis';
 
 #Invoice 
 
+#QUESTION:
 SELECT min(Total), max(Total), avg(Total) from Invoice WHERE BillingCountry='Germany';
 
+#QUESTION:
 SELECT InvoiceId, BillingCity FROM Invoice WHERE BillingCountry='France';
 
+#QUESTION:
 SELECT Name, Composer, Milliseconds FROM Track WHERE Milliseconds > 4000000;
 
