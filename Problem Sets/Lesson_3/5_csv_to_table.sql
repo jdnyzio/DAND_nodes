a967085061c044e3ca90fa1020b3eb70acdc3132
jdnyzio <joe.nyzio@udacity.com>
2015-10-20 19:54:40 +0000

    iron

--- Problem Sets/Lesson_3/5_csv_to_table.sql
+++ Problem Sets/Lesson_3/5_csv_to_table.sql
@@ -1,10 +1,6 @@
 sqlite> .exit
 $ sqlite3 over_avg_invoice.db
 
-sqlite> CREATE TABLE over_avg_invoice(FirstName TEXT, LastName TEXT, BillingCity TEXT, BillingState TEXT, BillingCountry TEXT, InvoiceTotal REAL);
-sqlite> .tables
-over_avg_invoice
-
 sqlite> .mode csv 
 sqlite> .separator ','
 sqlite> .import over_avg_invoice.csv over_avg_invoice
---
+++ Problem Sets/Lesson_3/6_create_table.sql
@@ -0,0 +1,11 @@
+#In this problem set, you'll be investigating your best customers.  
+#Who are they, where are they from, and what are they spending?
+#You'll use subqueries, joins, csvs, and your ability to create databases and tables to make it happen.
+
+#You've just seen how to export data into a csv, and how to import a csv into a database
+
+#What we want to do here is create a new database, and create a table to import your query into.
+
+#Create your table here
+
+... 
