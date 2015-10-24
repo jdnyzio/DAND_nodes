9b6750791d9c8a71785eca64fc8b529eb27e7995
jdnyzio <joe.nyzio@udacity.com>
2015-10-24 01:42:25 +0000

    invoiceline

--- Problem Sets/Lesson_2/5_playlist_join.sql
+++
@@ -1,7 +0,0 @@
-#Now try a JOIN on your own!
-
-#QUESTION: Build a query that gets the Playlist Name and TrackId
-#into the same table and lists them in descending order.
-
-#Your Query
-SELECT ...
---
+++ Problem Sets/Lesson_3/3_create_view.py
@@ -0,0 +1,31 @@
+'''
+In this problem set, you'll be investigating your best customers.  
+Who are they, where are they from, and what are they spending?
+You'll use subqueries, joins, csvs, and your ability to create databases and tables to make it happen.
+
+Now you'll want to create a view of your query to easily access this information.
+
+Here is an example of a view you've seen in class
+
+CREATE VIEW course_size as 
+SELECT course_id, COUNT(*) as num
+FROM enrollment
+GROUP by course_id;
+
+See if you can turn your query into a view!
+'''
+
+QUERY = '''
+SELECT ...
+
+'''
+
+
+
+
+
+
+
+
+
+
